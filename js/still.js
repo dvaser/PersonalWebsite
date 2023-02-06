47f4b84fa76e0da448dacfff6d17205a436d6f0b
Doğukan Vatansever <74770052+dvaser@users.noreply.github.com>
2023-02-06 14:42:01 +0300

    website

---
+++ js/still.js
@@ -0,0 +1,13 @@
+function changeNavBg(){
+    var nav = document.getElementById('nav-bar');
+    var scrollContent = document.getElementById('whoami');
+    let value = scrollContent.offsetTop-100;
+    var scrollValue = window.scrollY;
+    if (scrollValue >= value){
+        nav.classList.add('nav-color');
+    }
+    else{
+        nav.classList.remove('nav-color');
+    }
+}
+window.addEventListener('scroll', changeNavBg);
