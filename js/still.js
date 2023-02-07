e2c8686abe828cc06a4707007009475acafdf04c
Doğukan Vatansever <74770052+dvaser@users.noreply.github.com>
2023-02-07 09:05:57 +0300

    website

--- js/still.js
+++ js/still.js
@@ -1,13 +1,16 @@
 function changeNavBg(){
     var nav = document.getElementById('nav-bar');
-    var scrollContent = document.getElementById('whoami');
-    let value = scrollContent.offsetTop-100;
+    var scrollContent = document.getElementById('article');
+    let set = 50;
+    let value = scrollContent.offsetTop + set;
     var scrollValue = window.scrollY;
     if (scrollValue >= value){
-        nav.classList.add('nav-color');
+        nav.classList.remove('sticky-top');
+        nav.classList.add('fixed-top');
     }
     else{
-        nav.classList.remove('nav-color');
+        nav.classList.remove('fixed-top');
+        nav.classList.add('sticky-top');
     }
 }
 window.addEventListener('scroll', changeNavBg);
