31b484446596253f66a0166d948cc8db6bdda823
Doğukan Vatansever <74770052+dvaser@users.noreply.github.com>
2023-02-07 14:46:05 +0200

    Add files via upload

--- js/still.js
+++ js/still.js
@@ -1,5 +1,7 @@
 function changeNavBg(){
     var nav = document.getElementById('nav-bar');
+    var upIcon = document.getElementById('upIcon');
+
     var scrollContent = document.getElementById('article');
     let set = 50;
     let value = scrollContent.offsetTop + set;
@@ -7,10 +9,12 @@ function changeNavBg(){
     if (scrollValue >= value){
         nav.classList.remove('sticky-top');
         nav.classList.add('fixed-top');
+        upIcon.classList.remove('d-none');
     }
     else{
         nav.classList.remove('fixed-top');
         nav.classList.add('sticky-top');
+        upIcon.classList.add('d-none');
     }
 }
 window.addEventListener('scroll', changeNavBg);
